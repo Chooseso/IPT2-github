2394f76784d2474d37e9742d2d56d6e60b3912f4
Chooseso <calmj55@gmail.com>
2024-09-12 14:11:31 +0800

    Add subtraction method

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,14 @@ public class Calculator {
     public static double add (double a, double b){
         return a + b;
     }
+    public static double subtract (double a, double b){
+        return a - b;
+    }
+
     public static void main (String[] args){
         System.out.println("Simple Calculator")
         System.out.println("Addition: " + add(4,2));
+        System.out.println("Subtraction: " + subtract(9,6));
         
     }
 }
